48c456c61e9af0f0b1878d07da913ec2b5defffd
gmartine32 <gianfrancovillam@gmail.com>
2025-06-05 00:21:07 -0500

    feat: se añade loading al hacer login simulando una peticion http

--- src/components/SubmitedButton/SubmitedButton.tsx
+++ src/components/SubmitedButton/SubmitedButton.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 
-interface SubmitedButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+interface SubmitedButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  loading?: boolean;
 }
-const SubmitedButton: React.FC<SubmitedButtonProps> = ({ children, ...buttonProps }) => {
+const SubmitedButton: React.FC<SubmitedButtonProps> = ({
+  children,
+  loading,
+  ...buttonProps
+}) => {
   return (
     <button
       className="w-full rounded-2xl h-15 bg-primary hover:bg-tertiary hover:text-black cursor-pointer"
       {...buttonProps}
     >
-      {children}
+      {loading ? "Loading..." : children}
     </button>
   );
 };
